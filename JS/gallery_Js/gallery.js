c1de49df78002f595154c27bf72c95ae43abbb62
D ROHIT RAJAN <163647671+jannepureddytech@users.noreply.github.com>
2024-06-05 10:26:33 +0530

    Initial commit

---
+++ JS/gallery_Js/gallery.js
@@ -0,0 +1,14 @@
+// document.getElementById("viewAllBtn").addEventListener("click", function () {
+//   var galleryItems = document.querySelectorAll(".gallery-item");
+
+//   galleryItems.forEach(function (item) {
+//     item.classList.toggle("show");
+//   });
+
+//   var btn = document.getElementById("viewAllBtn");
+//   if (btn.innerText === "View All") {
+//     btn.innerText = "Hide All";
+//   } else {
+//     btn.innerText = "View All";
+//   }
+// });
